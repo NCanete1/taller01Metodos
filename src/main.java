6068d16414fb18cc2ec42ba0293d9c0ecbcfa94e
NCanete1 <101651736+NCanete1@users.noreply.github.com>
2022-03-23 10:31:00 -0300

    final

--- src/main.java
+++ src/main.java
@@ -5,43 +5,63 @@ public class main {
         int n = (int) (Math.random() * (max - min)) + min;
         return n;
     }
-    public static int CantProductos(){
+    public static int StockProductos(){
         int min = 0;
         int max = 15;
         int x = (int) (Math.random() * (max - min)) + min;
         return x;
     }
-    public static int[] ProductosCarro(int Numero1){
-        int[] cantidad= new int[Numero1];
-        for (int i = 0; i < Numero1; i++) {
-            cantidad[i] = CantProductos();
+    public static int[] ProductosCarro(){
+        int[] cantidad= new int[Numero1()];
+        for (int i = 0; i < cantidad.length; i++) {
+            cantidad[i] = StockProductos();
         }
         return cantidad;
     }
-    public static void MostrarProductos(){
-        int n = Numero1();
-        for(int i=0;i<n;i++){
-            System.out.println(ProductosCarro((n)));
+    public static int TotalProductos(){
+        int[] Productos= ProductosCarro();
+        int total=0;
+        for(int i=0;i<Productos.length;i++){
+            total= total+Productos[i];
+        }
+        return total;
+    }
+    public static void MostrarDetalle(int[] Productos){
+        for(int i=0;i<Productos.length;i++){
+            System.out.println("El producto ["+ (i+1) +"] cantidad:"+Productos[i]);
         }
     }
-    public static int[] ValorProductos(int cantidad) {
-        int[] precio = new int[cantidad];
-        for (int i = 0; i < cantidad; i++) {
+    public static int[] PrecioProductos() {
+        int[] precio = ProductosCarro();
+        for (int i = 0; i < precio.length; i++) {
             int n = i * 150;
             precio[i] = 500 + n;
         }
         return precio;
     }
-
-    public static int SumaProductos(int[] a, int b) {
-        int suma = 0;
-        for (int i = 0; i < b; i++) {
-            suma = a[i] + suma;
+    public static int TotalCompra(){
+        int [] precio = PrecioProductos();
+        int [] cantidad = ProductosCarro();
+        int total=0;
+        for (int i = 0; i < ProductosCarro().length; i++) {
+            total = total+ (precio[i] * cantidad[i]);
         }
-        return suma;
+        return total;
     }
 
+    public static void Ejecutar() {
+        int [] precio = PrecioProductos();
+        int [] cantidad = ProductosCarro();
+        int total=0;
+        for (int i = 0; i < ProductosCarro().length; i++) {
+            System.out.println("Producto ["+ (i+1)+ "]" +": "+ cantidad[i]+ " Precio: "+ (cantidad[i]* precio[i]));
+            System.out.println("El total de productos = "+ TotalProductos());
+
+            total = total+ (precio[i] * cantidad[i]);
+        }
+        System.out.println(total);
+    }
     public static void main(String[] args) {
-     MostrarProductos();
+        Ejecutar();
     }
 }
